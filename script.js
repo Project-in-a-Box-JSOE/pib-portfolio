0db0e7d963667a5a1bd4ca41b11290eda47cc7dd
Jesse Ren <jeren.neurogen@gmail.com>
2017-09-24 02:40:47 -0700

    Added Banner Picture

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 document.querySelector("body").style.padding = "80px 10%";
 document.querySelector("#content").style.width = "80%";
 document.querySelector("#content").style.width = "80%";
+//
+// var parent = document.getElementsByClassName("container")[1];
+// var firstChild = parent.firstChild;
+// // nav.style.backgroundColor = "red";
+// var img = document.createElement("img");
+// img.src = "/images/banner.png";
+// img.style.width = "100%";
+// // parent.append(img);
+// // parent.insertBefore(img, parent);
+//
+//
+// var nav = document.getElementsByClassName("container")[0];
+// var container = document.createElement("div");
+// container.className = "floater-right";
+//
+// var links = [
+//   "word",
+//   "Something",
+//   "word"
+// ]
+//
+// for (var i = 0; i < links.length; i++) {
+//   var elem = document.createElement("a");
+//   elem.href = "google.com";
+//   elem.innerHTML = links[i];
+//   elem.className = "link";
+//   container.append(elem);
+// }
+//
+// nav.append(container);
